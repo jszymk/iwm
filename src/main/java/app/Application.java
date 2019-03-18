73c057a981e185aa7187135e55a15dac87b35b77
jszymk <janusz.szymkowiak1@gmail.com>
2019-03-18 03:20:28 +0100

    merge

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,16 +1,24 @@
 package app;
 
 import app.controller.MainViewConroller;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import app.conversion.InverseRadonTransform;
+import app.conversion.RadonTransform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
 
 public class Application extends javafx.application.Application {
    
@@ -19,7 +27,21 @@ public class Application extends javafx.application.Application {
     private BorderPane rootLayout;
 
     //public static FXMLLoader loader = new FXMLLoader();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        File input_file = new File("inp.jpg");
+
+        BufferedImage image = ImageIO.read(input_file);
+
+        BufferedImage sin = new RadonTransform(400, 90, 0.5).transform(image);
+
+        File f = new File("sin.jpg");
+        ImageIO.write(sin, "jpg", f);
+
+        BufferedImage out = new InverseRadonTransform(400, 90, 0.5).transform(sin, 5);
+
+        f = new File("out.jpg");
+        ImageIO.write(out, "jpg", f);
+
         launch(args);
     }
 
